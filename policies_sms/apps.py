9923c24009ab0d3df499b060ed3d222681342320
Damian Borowiecki <dborowiecki@soldevelo.com>
2021-05-31 16:09:03 +0200

    OP-349: Include eGASMSGateway configuration

--- policies_sms/apps.py
+++ policies_sms/apps.py
@@ -7,6 +7,17 @@ DEFAULT_CONFIG = {
     "providers": {
         "TextSMSProvider": {
             "DestinationFolder": "sent_sms"
+        },
+        "eGASMSGateway": {
+            "GateUrl": "http://127.0.0.1:8000",
+            "SmsResource": "/api/policies_sms/test_sms/",
+            "PrivateKey": "",
+            "UserId": "",
+            "SenderId": "",
+            "ServiceId": "",
+            "RequestType": "",
+            "HeaderKeys": "X-Auth-Request-Hash,X-Auth-Request-Id,X-Auth-Request-Type",
+            "HeaderValues": "PrivateKey,UserId,RequestType"
         }
     }
 }
@@ -15,7 +26,7 @@ DEFAULT_CONFIG = {
 class PoliciesSmsConfig(AppConfig):
     name = MODULE_NAME
 
-    providers = []
+    providers = {}
 
     def _configure_perms(self, cfg):
         PoliciesSmsConfig.providers = cfg["providers"]
